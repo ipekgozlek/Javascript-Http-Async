4925d2ea867f376f2df0d59d76b5970959ea590c
İpek Gözlek <ipekgozlek@gmail.com>
2025-11-17 23:30:17 +0300

    ufak eklemeler

--- 03-Fetch/fetch.js
+++ 03-Fetch/fetch.js
@@ -1,4 +1,14 @@
-function getTextFile() {
-    fetch("text.txt").then((response) => console.log(response));
-}
+// function getTextFile() {
+//     fetch("text.txt").then((response) => console.log(response.text()))
+//     .then((data) => console.log(data))
+//     .catch((error) => console.error(error));
+// }
+// // fetch ettiğim dosyayı response olarak cevabı aldım ve text() metodu ile dosyanın içeriğini okuyorum
+// //sonra o içeriği data olarak alıp console.log ile yazdırıyorum
 
+//  getTextFile();
+
+function getJSONFile() {
+    fetch("users.json").then((response)=> response.json()).then((data) => console.log(data));
+}
+getJSONFile();
